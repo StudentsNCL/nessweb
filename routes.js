25f2a583fc225d1d19cf83efa7f53ed197dd091c
Mark Hemmings <mark@mhemmings.com>
2014-05-08 22:01:03 +0100

    Check for failed auth on each request

--- routes.js
+++ routes.js
@@ -3,6 +3,10 @@ var ness = require('nessjs'),
 
 exports.index = function(req, res) {
     ness.getName(function(err, name) {
+        if (err) {
+            req.session.failed_login = true;
+            return res.redirect('/login');
+        }
         res.render('index', {name: name});
     });
 };
@@ -19,6 +23,10 @@ exports.login_post = function(req, res) {
 
 exports.modules = function(req, res) {
     ness.getModules('attendance', function(err, modules) {
+        if (err) {
+            req.session.failed_login = true;
+            return res.redirect('/login');
+        }
         res.render('modules', {modules: modules});
     });
 };
