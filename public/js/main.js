ad028dd185c6a96a14327ca32adae6f0aa06e090
James McLaughlin <jamie@lacewing-project.org>
2014-05-10 17:17:38 +0100

    Add tooltips

--- public/js/main.js
+++ public/js/main.js
@@ -7,9 +7,15 @@ $(function()
         {
             this.style.width = $(this).attr('data-width') + '%';
         });
+
     }, 300);
+
+    $('*[title]').tooltip();
+
+
 });
 
 $('body').on('hidden.bs.modal', '.modal', function () {
     $(this).removeData('bs.modal');
 });
+
