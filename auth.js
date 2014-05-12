StudentsNCL/nessweb

commit c2a11664afd9cc875967f2d3abc8af0bd0f6fb65

Improve session management

auth.js
@@ -1,11 +1,13 @@
 var ness = require('nessjs');
+
 module.exports = function (req, res, next) {
-    if (!req.session.user || !req.session.user.name) {
+    if (!req.session.user) {
         res.redirect('/login');
         req.session.referer = req.originalUrl;
         return;
     }
 
+    req.session.touch();
     res.locals.user = req.session.user;
 
     next();
@@ -19,9 +21,17 @@ module.exports.login = function (req, res, onError) {
 
     req.session.user = user;
 
+    if (req.body.remember) {
+        res.cookie('ness_id', user.id, { httpOnly: true });
+    }
+    else {
+        res.clearCookie('ness_id');
+    }
+
     ness.getName(user, function(err, name) {
         if (err) {
             req.session.failed_login = true;
+            req.session.failed_id = user.id;
             return res.redirect('/login');
         }
         req.session.user.name = name;
@@ -32,10 +42,10 @@ module.exports.login = function (req, res, onError) {
 }
 
 module.exports.logout = function (forced, req, res) {
-    req.session.failed_login = forced;
-    var id = req.session.user.id;
-    req.session.user = {id: id};
-    res.redirect('/login');
+    req.session.regenerate(function () {
+        req.session.failed_login = forced;
+        res.redirect('/login');
+    });
 }
 
 module.exports.isLoggedIn = function (req) {

routes.js
@@ -8,7 +8,11 @@ exports.login_get = function(req, res) {
     }
     var failed_login = req.session.failed_login;
     req.session.failed_login = false;
-    res.render('login', {layout: 'login', failed: failed_login, user: req.session.user});
+    var failed_id = req.session.failed_id;
+    req.session.failed_id = '';
+    var id = failed_login ? failed_id : req.cookies.ness_id;
+
+    res.render('login', {layout: 'login', failed: failed_login, ness_id: id});
 };
 
 exports.login_post = function(req, res) {

server.js
@@ -11,7 +11,7 @@ var app = express();
 
 app.use(express.static('public'));
 app.use(cookieParser())
-app.use(session({secret: 'winter is coming' }));
+app.use(session({secret: 'winter is coming', key: 'nessweb_sid', cookie: {maxAge: 30 * 60 * 1000} }));
 app.use(bodyParser());
 
 app.engine('.hbs', handlebars({

views/login.hbs
@@ -9,11 +9,16 @@
                 {{#if failed}}<div class="alert alert-danger">We had trouble logging you in. Please try again.</div>{{/if}}
                 <div class="form-group">
                     <label for="user">Username:</label>
-                    <input class="form-control" type="text" name="id" placeholder="b20xxxxx" value="{{user.id}}" id="user" required/>
+                    <input class="form-control" type="text" name="id" placeholder="b20xxxxx" value="{{ness_id}}" id="user" required/>
                 </div>
                 <div class="form-group">
                     <label for="password">Password:</label>
                     <input class="form-control" type="password" name="pass" id="password" placeholder="Password" required/>
+                </div>
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="remember" checked="checked"> Remember Me
+                    </label>
                 </div>
                 <button class="btn btn-lg btn-primary btn-block" type="submit">Log In</button>
             </form>

commit 85dc3c6f0cecfd7cce2350836544ad53eb826581

Fix minor bug introduced in c2a1166

auth.js
@@ -1,7 +1,7 @@
 var ness = require('nessjs');
 
 module.exports = function (req, res, next) {
-    if (!req.session.user) {
+    if (!req.session.user || !req.session.user.name) {
         res.redirect('/login');
         req.session.referer = req.originalUrl;
         return;