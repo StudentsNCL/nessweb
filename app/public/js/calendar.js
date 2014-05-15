27f7ea7205f461ee8bd3a9d908cb532d917d7167
Alex Brazier <aejbrazier@gmail.com>
2014-05-15 17:51:00 +0100

    Add calendar navigation buttons

--- app/public/js/calendar.js
+++ app/public/js/calendar.js
@@ -2,5 +2,37 @@
 var calendar = $("#calendar").calendar(
     {
         tmpl_path: "/tmpls/",
-        events_source: '/json/calendar'
+        events_source: '/json/calendar',
+        onAfterEventsLoad: function(events) {
+			if(!events) {
+				return;
+			}
+			var list = $('#eventlist');
+			list.html('');
+
+			$.each(events, function(key, val) {
+				$(document.createElement('li'))
+					.html('<a href="' + val.url + '">' + val.title + '</a>')
+					.appendTo(list);
+			});
+		},
+		onAfterViewLoad: function(view) {
+			$('.page-header h3').text(this.getTitle());
+			$('.btn-group button').removeClass('active');
+			$('button[data-calendar-view="' + view + '"]').addClass('active');
+		}
     });
+
+$('.btn-group button[data-calendar-nav]').each(function() {
+	var $this = $(this);
+	$this.click(function() {
+		calendar.navigate($this.data('calendar-nav'));
+	});
+});
+
+$('.btn-group button[data-calendar-view]').each(function() {
+	var $this = $(this);
+	$this.click(function() {
+		calendar.view($this.data('calendar-view'));
+	});
+});
